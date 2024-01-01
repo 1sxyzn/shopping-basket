a10afe1301e9a784b076569c67f4cdfd4a0afa53
1sxyzn <soyun9902@ewhain.net>
2024-01-02 01:19:50 +0900

    model

--- src/models.py
+++ src/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.TextField()
+    desc = models.TextField()
+    price = models.IntegerField()
+
+
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    cnt = models.IntegerField(default=0)
+
+
+class Basket(models.Model):
+    order = models.ManyToManyField(Order)
